d4637c6a44933eb7d77717af5f21990ff88d3bb4
Matt <mattmartino54@gmail.com>
2022-11-16 13:36:34 -0600

    Added scotts other delete button while he was interviewing very well, added routs and options to edit tags and categories.

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,12 +1,22 @@
 import "./Category.css"
 import { Link } from "react-router-dom"
+import { deleteCategory } from "../../managers/CategoryManager"
+import { CategoryEdit } from "./CategoryEdit"
+
 
 export const Category = ({ category }) => (
     <section className="block">
         <div className="tag is-large">
-            <Link to={`/categories/${category.id}`}>
+            <Link to={`/categories/${category.id}/edit`}>
                 { category.label }
             </Link>
+            <button
+                  className="btn btn-primary"
+                  onClick={() => deleteCategory(category.id).then(window.location.reload(false))}
+                >
+                  Delete
+            </button>
+            
         </div>
         
     </section>
