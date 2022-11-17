872a31c9bb5e46896024250ad683dd5da70963e7
Matt <mattmartino54@gmail.com>
2022-11-17 15:36:11 -0600

    End of day commit.  Added the ability to rout out of post details page to a post comments page viewing comments.  Needs more work, but is functional.

--- src/components/posts/postDetails.js
+++ src/components/posts/postDetails.js
@@ -12,89 +12,90 @@ export const PostDetails = () => {
     getPostById(postId).then(postData => setPost(postData))
   }, [postId])
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
 
-    const renderDeleteButton = (postId) => {
-      return <>
+  const renderDeleteButton = (postId) => {
+    return <>
       <button className="button is-small is-danger is-focused"
         onClick={() => {
-            if (window.confirm('Are you sure you want to delete this post?')) {
-                makeDeleteRequest(postId)
-            }
+          if (window.confirm('Are you sure you want to delete this post?')) {
+            makeDeleteRequest(postId)
+          }
         }}
-        >
-            <i className="fa-solid fa-trash-can"></i>
-        </button>
-      </>
-    }
+      >
+        <i className="fa-solid fa-trash-can"></i>
+      </button>
+    </>
+  }
 
   const makeDeleteRequest = (postId) => {
-        deletePost(postId)
-        .then(() => {
-         navigate("/allposts")
-       })
-    }
+    deletePost(postId)
+      .then(() => {
+        navigate("/allposts")
+      })
+  }
 
 
-    const displayPostDetailsCard = () => {
+  const displayPostDetailsCard = () => {
     return <>
-    <section className="columns">
-    <div class="card column">
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt=""/>
-    </figure>
-  </div>
-  <div class="card-content">
-    <div className="columns">
-        <div className="column">
-    <div class="media">
-      <div class="media-left">
-        <figure class="image is-48x48">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt="" />
-        </figure>
-      </div>
-      <div class="media-content">
-        <div>
-        <Link className="title is-4 color is-primary" to={`/userprofile/${post.user_id}`} >{post?.user?.full_name}</Link>
+      <section className="columns">
+        <div class="card column">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="" />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div className="columns">
+              <div className="column">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img src="https://bulma.io/images/placeholders/96x96.png" alt="" />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <div>
+                      <Link className="title is-4 color is-primary" to={`/userprofile/${post.user_id}`} >{post?.user?.full_name}</Link>
+                    </div>
+                    <div>
+                      <p class="subtitle is-6">@{post?.user?.username}</p>
+                    </div>
+                    <div>
+                      <p class="subtitle is-6">{post.publication_date}</p>
+                    </div>
+                    {renderDeleteButton(postId)}
+                  </div>
+                </div>
+              </div>
+              <div className="column is-one-third">
+                <div className="title is-5 level-item">Reactions</div>
+                <div className="box">{post?.reaction?.emoji}</div>
+                <button className="button is-primary">
+                  <Link to={`/posts/${postId}/comments`}>View Post Comments</Link></button>
+              </div>
+            </div>
+            <div class="content">
+              {post.content}
+            </div>
+          </div>
         </div>
-        <div>
-        <p class="subtitle is-6">@{post?.user?.username}</p>
+        <div className="column is-one-fifth box">
+          <div className="level">
+            <h3 className="title is-5 level-item">Category</h3>
+          </div>
+          <div className="level-item">
+            <div>{post?.category?.label}</div>
+          </div>
+          <br></br>
+          <div className="level">
+            <h3 className="title is-5 level-item">Tags</h3>
+          </div>
+          <div className="level-item">
+            {post?.tag?.label}
+          </div>
         </div>
-        <div>
-        <p class="subtitle is-6">{post.publication_date}</p>
-        </div>
-        {renderDeleteButton(postId)}
-      </div>
-    </div>
-  </div>
-  <div className="column is-one-third">
-    <div className="title is-5 level-item">Reactions</div>
-    <div className="box">{post?.reaction?.emoji}</div>
-<button className="button is-primary">View Comments</button>
-  </div>
-  </div>
-   <div class="content">
-      {post.content}
-    </div>
-</div>
-</div>
-<div className="column is-one-fifth box">
-    <div className="level">
-    <h3 className="title is-5 level-item">Category</h3>
-    </div>
-    <div className="level-item">
-    <div>{post?.category?.label}</div>
-    </div>
-    <br></br>
-    <div className="level">
-    <h3 className="title is-5 level-item">Tags</h3>
-    </div>
-    <div className="level-item">
-    {post?.tag?.label}
-    </div>
-</div>
 
       </section>
 
